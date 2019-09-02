33251d030153d44b59a50e67cea0452acfb4d533
anuj1560 <41946647+anuj1560@users.noreply.github.com>
2019-09-03 03:30:28 +0530

    Create JumpOut.cpp

---
+++ JumpOut.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	    int n;
+	    cin >>n;
+	    int a[n];
+	    for(int i=1;i<=n;i++)
+	    cin >>a[i];
+	    for(int i=1;i<=n;i++){
+	        if((i+a[i])>n){
+	            cout<<i<<endl;
+	            break;
+	        }
+	        }
+
+    return 0;
+}
