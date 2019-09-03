8275e600da1a1b15531b7cf9c9d58e661829cfef
anuj1560 <41946647+anuj1560@users.noreply.github.com>
2019-09-04 00:06:38 +0530

    Create PradyumanAndCyclicString.cpp

---
+++ PradyumanAndCyclicString.cpp
@@ -0,0 +1,17 @@
+#include <iostream>  
+#define ll unsigned long long int
+using namespace std;  
+  
+  int main()  
+  {  
+    int t;  
+    cin>>t;  
+  while(t--){  
+      string s;  
+      cin>>s;  
+      ll n = s.size();  
+     cout<<(n*(n+1))/2<<endl;  
+    }  
+    
+    return 0;  
+  }
